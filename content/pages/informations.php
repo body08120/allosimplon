213bac8ec689669a128eb825ca6791c617d4069d
body08120 <nataelrenollet08120@gmail.com>
2023-03-17 08:35:20 +0100

    commit info

---
+++ content/pages/informations.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AlloSimplon - Informations</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css"  rel="stylesheet" />
+</head>
+<body class="bg-[#412234] font-mono">
+
+<!--navbar-->
+<?php include('../includes/navbar.php') ?>
+
+
+<!-- text contact-->
+<div class="py-5 px-10">
+
+    <h1 class="text-center uppercase font-semibold text-[24px] text-[#EAD7D7] tracking-[.15em]">Informations utiles<h1>
+
+    <p class="py-5 text-center font-light text-[#EAD7D7] text-[18px] tracking-[.10em]">
+    Bienvenue sur notre site de streaming gratuit! Nous sommes ravis de vous proposer une expérience de visionnage en ligne de haute qualité en 2023 et au-delà.
+    </p>
+
+    <p class="py-5 text-center font-light text-[#EAD7D7] text-[18px] tracking-[.10em]">
+    Notre plateforme est constamment mise à jour pour répondre aux besoins et aux attentes de nos utilisateurs, 
+    et de nombreuses améliorations et fonctionnalités utiles sont à venir dans un proche avenir.   
+    </p>
+
+    <p class="py-5 text-center font-light text-[#EAD7D7] text-[18px] tracking-[.10em]">
+    Nous sommes fiers de dire que notre site est développé par Nataël Renollet, 
+    un expert en développement web avec des années d'expérience dans la création de plateformes de streaming. 
+    Nous sommes convaincus que vous apprécierez l'expérience que nous avons créée pour vous, 
+    avec une sélection de films et de séries TV captivants que vous pouvez regarder gratuitement et en ligne. 
+    </p>
+
+    <p class="py-5 text-center font-light text-[#EAD7D7] text-[18px] tracking-[.10em]">
+    Nous sommes engagés à fournir à nos utilisateurs une expérience de streaming sans interruption et de qualité supérieure, 
+    et nous sommes toujours à l'écoute de vos commentaires et de vos suggestions pour améliorer notre site. 
+    Merci d'avoir choisi notre site pour vos besoins de streaming et nous avons hâte de continuer à vous proposer les meilleurs contenus en ligne!  
+    </p>
+
+</div>
+
+
+<!-------------------------------------->
+<div class="h-1 divide-[#412234]"></div>
+<!--footer-->
+<?php include('../includes/footer.php') ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
+</body>
+</html>
