7eadb5985e34f225d1028b77bd0245fe32d6e9cd
body08120 <nataelrenollet08120@gmail.com>
2023-03-22 08:50:56 +0100

    config db commit

---
+++ content/formulaires-traitement/config.php
@@ -0,0 +1,29 @@
+<?php
+// Paramètres de connexion à la base de données
+define('DB_HOST', 'localhost'); // nom de l'hôte
+define('DB_NAME', 'allosimplon-db'); // nom de la base de données
+define('DB_USER', 'root'); // nom d'utilisateur de la base de données
+define('DB_PASSWORD', ''); // mot de passe de l'utilisateur de la base de données
+
+// Connexion à la base de données avec PDO
+try {
+    $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASSWORD);
+    // Si une erreur survient, on configure PDO pour qu'il renvoie une exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données: " . $e->getMessage();
+    die();
+} 
+
+
+
+// Vérification pour afficher un message indiquant si la connexion à la base de données a réussi ou échoué :
+// if ($pdo->errorCode() !== null) {
+//   echo "Erreur de connexion à la base de données: ";
+//   $error = $pdo->errorInfo();
+//   echo $error[2];
+//   die();
+// } else {
+//   echo "Connexion à la base de données réussie.";
+// }
+?>
