e79157365b467bfaa1a668464d77d779499b5bcb
body08120 <nataelrenollet08120@gmail.com>
2023-04-05 08:53:34 +0200

    ajust

--- assets/js/cata-search.js
+++ assets/js/cata-search.js
@@ -1,11 +1,15 @@
+// On vise le formulaire et la zone d'affichage
 const searchForm = document.querySelector('#form-search form');
 const resultZone = document.querySelector('#result-zone');
 
+//on ajout un event à submit
 searchForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Empêche le formulaire de s'envoyer
 
+    //On vise l'input 
     const searchValue = searchForm.elements['search-input-catalogue'].value;
     const xhr = new XMLHttpRequest();
+    //On renvoie l'url en get
     xhr.open('GET', `content/formulaires-traitement/search.php?search-input-catalogue=${searchValue}`);
     xhr.onload = function() {
         if (xhr.status === 200) {
