6796984d07a412c4e2fab37ee6b3200d76b4bd2d
body08120 <nataelrenollet08120@gmail.com>
2023-04-06 18:46:37 +0200

    lien

--- index.php
+++ index.php
@@ -52,27 +52,27 @@ $films = $stmt->fetchAll();
     <div class="relative h-56 overflow-hidden md:h-96 lg:h-[38rem] xl:h-[48rem]">
       <!-- Item 1 -->
       <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="/projects/allosimplon/assets/img/carousel/carousel-1.png"
+        <img src="/projets/allosimplon/assets/img/carousel/carousel-1.png"
           class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
       </div>
       <!-- Item 2 -->
       <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-        <img src="/projects/allosimplon/assets/img/carousel/carousel-2.png"
+        <img src="/projets/allosimplon/assets/img/carousel/carousel-2.png"
           class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
       </div>
       <!-- Item 3 -->
       <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="/projects/allosimplon/assets/img/carousel/carousel-3.png"
+        <img src="/projets/allosimplon/assets/img/carousel/carousel-3.png"
           class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
       </div>
       <!-- Item 4 -->
       <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="/projects/allosimplon/assets/img/carousel/carousel-4.png"
+        <img src="/projets/allosimplon/assets/img/carousel/carousel-4.png"
           class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
       </div>
       <!-- Item 5 -->
       <div class="hidden duration-700 ease-in-out" data-carousel-item>
-        <img src="/projects/allosimplon/assets/img/carousel/carousel-5.png"
+        <img src="/projets/allosimplon/assets/img/carousel/carousel-5.png"
           class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
       </div>
     </div>
